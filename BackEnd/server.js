30115941cf46e94e1de0449f48e53806d59ed45c
NAGARAJ8888 <nagaraj81471@gmail.com>
2025-06-22 21:08:06 +0530

    backend test

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,10 @@ const app = require('./src/app')
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/',(req, res)=>{
+    res.send('server is running');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
