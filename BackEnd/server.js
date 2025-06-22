b06ce8e5da3a94559a0fb0d2bda34ed9e876145b
NAGARAJ8888 <nagaraj81471@gmail.com>
2025-06-22 22:04:29 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,9 +3,7 @@ const app = require('./src/app')
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/',(req, res)=>{
-    res.send('server is running');
-});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
